49eab06bae8966db57f1de4b9cab661f0e511990
Shampiii <98670896+DulceNev@users.noreply.github.com>
2026-03-18 18:15:41 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex flex-col items-center justify-center h-screen p-4">
+    <h1 class="text-2xl font-bold">Base app</h1>
+    <p class="text-gray-300 text-center">esta es una base en electron con vue que usaré para mis proyectos y no empezar de 0 siempre</p>
+    
+  </div>
+</template>
+
+<script setup>
+
+</script>
