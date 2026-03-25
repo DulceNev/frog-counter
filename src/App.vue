88991f01136aa3de8563df149cda115d337079fd
DulceNev <dulcenev04@gmail.com>
2026-03-24 20:40:49 -0500

    se agregaron los botones y funcionalidades para sumar y restar al contador

--- src/App.vue
+++ src/App.vue
@@ -30,13 +30,18 @@ import { ref } from 'vue';
       <p class="font-bold text-xl">Frog Counter</p>
     </header>
     <main class="flex justify-center items-center">
-      <img src="" alt="">
+      <button @click="decrement">
+        <img class="transition-transform duration-100 ease-out hover:scale-110 active:scale-90" src="./assets/svg/minus.svg" alt="minus-image">
+      </button>
       <div class="flex flex-col justify-center items-center bg-light-green w-50 h-50 rounded-4xl border-4 border-base">
         <p class="text-base font-extrabold text-xl">{{ stitchGuide }}</p>
         <p class="text-base font-extrabold text-[70px]">
           {{ count }}</p>
         <p class="text-base font-extrabold text-xl">{{count}} / {{target}}</p>
       </div>
+      <button @click="increment">
+        <img class="transition-transform duration-100 ease-out hover:scale-110 active:scale-90" src="./assets/svg/plus.svg" alt="plus-image">
+      </button>
       
     </main>
     
